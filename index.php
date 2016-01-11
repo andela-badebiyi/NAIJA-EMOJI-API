8df0965992a6bf3d8c032ce7a917ac1572cc776c
Adebiyi Bodunde <bodunde.adebiyi@andela.com>
2016-01-11 05:25:21 -0500

    Applied fixes from StyleCI

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <?php
 
-require "vendor/autoload.php";
+require 'vendor/autoload.php';
 
 //initialize db handlers;
 
 $db_config = parse_ini_file('config.ini');
 
-$dsn = ($db_config['db_location'] == 'remote') ? 
-"pgsql:host=".$db_config['host'].
-";dbname=".$db_config['db'].
-";user=". $db_config['user'].
-";password=".$db_config['pword'] : 'sqlite:test.db';
+$dsn = ($db_config['db_location'] == 'remote') ?
+'pgsql:host='.$db_config['host'].
+';dbname='.$db_config['db'].
+';user='.$db_config['user'].
+';password='.$db_config['pword'] : 'sqlite:test.db';
 $pdo = new PDO($dsn);
-$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $db = new NotORM($pdo);
 
 //initialize slim
 $app = new Slim\Slim();
 
-
 //secret hash key and the encryption method
-$secret_hash = "123ophiophagus!@#";
-$encryptionMethod = "AES-256-CBC"; 
+$secret_hash = '123ophiophagus!@#';
+$encryptionMethod = 'AES-256-CBC';
 
 //initializing vector;
 $iv = openssl_random_pseudo_bytes(16);
@@ -39,7 +38,7 @@ $app->get('/auth/logout', $action->authLogout());
 //fetch all emojis
 $app->get('/emojis/', $action->fetchAllEmojis());
 
-//fetch emoji by id 
+//fetch emoji by id
 $app->get('/emojis/:id', $action->fetchEmoji());
 
 //create a new emoji
@@ -49,16 +48,14 @@ $app->post('/emojis/', $action->createEmoji());
 $app->put('/emojis/:id/', $action->updateEmoji());
 
 //update an emoji using patch
-$app->patch('/emojis/:id/',$action->updateEmoji());
+$app->patch('/emojis/:id/', $action->updateEmoji());
 
 //delete an emoji
-$app->delete('/emojis/:id/',$action->deleteEmoji());
+$app->delete('/emojis/:id/', $action->deleteEmoji());
 
-//index page 
-$app->get('/', function() {
-   echo "Welcome to the naija emoji api homepage";
+//index page
+$app->get('/', function () {
+   echo 'Welcome to the naija emoji api homepage';
 });
 //run $app
 $app->run();
-
-?>
